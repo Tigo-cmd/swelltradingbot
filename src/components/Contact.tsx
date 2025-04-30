05ee4fec20177521e4e6eebc3bb13e1990c08320
Tigo-cmd <ja16162040@gmail.com>
2025-04-30 02:41:50 +0100

    added animations to icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,35 @@
 
 import React from 'react';
 import { Mail, Linkedin, Github, Twitter } from 'lucide-react';
+import FadeInSlide from "@/components/FadeInSlide";
+import FadeInWhenVisible from "@/components/Fadeinwhenvisible";
 
 const Contact = () => {
   return (
     <section id="contact" className="py-16 bg-main-gradient-reverse">
       <div className="swell-container text-center">
         <h2 className="font-freckle text-4xl text-navy mb-8">Contact Us</h2>
+
         
         <div className="flex justify-center space-x-6">
-          <a href="#" className="w-12 h-12 rounded-full bg-red-100 flex items-center justify-center text-red-500 hover:bg-red-200 transition-colors">
+         <FadeInSlide direction="left" >
+          <a href="https://swelltradingbot@gmail.com" target='_blank' className="w-12 h-12 rounded-full bg-red-100 flex items-center justify-center text-red-500 hover:bg-red-200 transition-colors">
             <Mail size={24} />
           </a>
+          </FadeInSlide>
           {/* <a href="#" className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center text-blue-600 hover:bg-blue-200 transition-colors">
             <Linkedin size={24} />
           </a> */}
-          <a href="#" className="w-12 h-12 rounded-full bg-neutral-100 flex items-center justify-center text-neutral-700 hover:bg-neutral-200 transition-colors">
+          <FadeInWhenVisible>
+          <a href="https://github.com/SwellTradingBot/" target='_blank' className="w-12 h-12 rounded-full bg-neutral-100 flex items-center justify-center text-neutral-700 hover:bg-neutral-200 transition-colors">
             <Github size={24} />
           </a>
-          <a href="#" className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center text-blue-400 hover:bg-blue-200 transition-colors">
+          </FadeInWhenVisible>
+          <FadeInSlide direction="right" >
+          <a href="https://x.com/swelltradingbot" target="_blank" className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center text-blue-400 hover:bg-blue-200 transition-colors">
             <Twitter size={24} />
           </a>
+          </FadeInSlide>
         </div>
       </div>
     </section>
